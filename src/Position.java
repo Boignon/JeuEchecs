cd1a0fa7b983d6ef9105388ff67896765c58b897
B2AC <boignonange70@gmail.com>
2026-06-03 01:43:17 +0000

    Création de la classe Position

---
+++ src/Position.java
@@ -0,0 +1,10 @@
+//creation la de la classe Position
+public class Position {
+    int ligne;
+    int colone;
+
+    public Position(int row, int column) {
+        this.ligne = row;
+        this.colone = column;
+    }
+}
