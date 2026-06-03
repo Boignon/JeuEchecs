11e004faec2c52a148b31c4b844d6ebf374b077b
B2AC <boignonange70@gmail.com>
2026-06-03 01:52:53 +0000

    verification du fonctionnement de Reine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args) {
+        // Création de la l'objet queen
+        Reine queen = new Reine();
+        queen.position = new Position(4, 4);
 
+        // Test d'un déplacement
+        Position test = new Position(6, 6);
+
+        if (queen.isValidMove(test)) {
+            System.out.println("Yes, I can move there.");
+        } else {
+            System.out.println("Nope, can't do!");
+        }
     }
 }
